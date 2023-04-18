6f0b544d00135253574f0a5a8ce32c0d17fea230
panyanyany <568397440@qq.com>
2023-04-18 22:53:05 +0800

    调整域名

--- src/config/demo.app.ts
+++ src/config/demo.app.ts
@@ -1,4 +1,4 @@
 export const AppConfig = {
-    API_ENDPOINT: process.env.NODE_ENV == 'production' ? 'https://api.example.com/api' : 'http://api:8080/api',
+    API_ENDPOINT: process.env.NODE_ENV == 'production' ? 'https://api.example.com/api' : 'http://127.0.0.1:8080/api',
 }
 
