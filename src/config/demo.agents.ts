011c260e488d94995e0eee995e8c003c03d4cf7c
panyanyany <568397440@qq.com>
2023-04-19 21:58:09 +0800

    add link

--- src/config/demo.agents.ts
+++ src/config/demo.agents.ts
@@ -3,6 +3,6 @@ import {Agent} from "../models/agent";
 export const agents: { [name: string]: Agent } = {
     cc: {
         wechat: 'redcover123',
-        adLink1: 'https://pr.kuaifaka.net/',
+        adLink1: 'http://www.kuaifaka.net/invitied?code=23957281',
     },
 }
