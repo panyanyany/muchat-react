553aa1211481078f35d40d864523a48a6b6329d3
panyanyany <568397440@qq.com>
2023-04-19 21:59:35 +0800

    add two ad

--- src/config/demo.agents.ts
+++ src/config/demo.agents.ts
@@ -1,8 +1,10 @@
 import {Agent} from "../models/agent";
 
 export const agents: { [name: string]: Agent } = {
-    cc: {
-        wechat: 'redcover123',
+    faka: {
         adLink1: 'http://www.kuaifaka.net/invitied?code=23957281',
+    },
+    wx: {
+        wechat: 'redcover123',
     },
 }
