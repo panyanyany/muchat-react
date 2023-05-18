89746e558b4224b6c22db86a1b94cde7a8675750
panyanyany <panyanyany@gmail.com>
2023-05-18 11:11:45 +0800

    REACT_APP_API_ENDPOINT

--- src/config/demo.app.ts
+++ src/config/demo.app.ts
@@ -1,5 +1,4 @@
 export const AppConfig = {
     // API_ENDPOINT: process.env.NODE_ENV == 'production' ? 'https://api.example.com/api' : 'http://127.0.0.1:8080/api',
-    API_ENDPOINT: `//${window['REACT_APP_HOSTNAME'] || process.env.REACT_APP_HOSTNAME}:8080/api`,
+    API_ENDPOINT: window['REACT_APP_API_ENDPOINT'],
 }
-
